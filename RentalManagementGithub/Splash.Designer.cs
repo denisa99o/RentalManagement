f67733ebeff4935075153aa75b9a5d5541ca1931
denisa99o <80585646+denisa99o@users.noreply.github.com>
2023-02-23 22:19:19 +0200

    Add files via upload

--- RentalManagementGithub/Splash.Designer.cs
+++ RentalManagementGithub/Splash.Designer.cs
@@ -34,17 +34,15 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.Percentage = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(156, 376);
+            this.progressBar1.Location = new System.Drawing.Point(170, 392);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(897, 32);
+            this.progressBar1.Size = new System.Drawing.Size(979, 33);
             this.progressBar1.TabIndex = 1;
             // 
             // label1
@@ -52,7 +50,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 14.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(386, 54);
+            this.label1.Location = new System.Drawing.Point(421, 56);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(346, 40);
             this.label1.TabIndex = 2;
@@ -61,9 +59,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::RentalManagement.Properties.Resources.bugattiCar;
-            this.pictureBox1.Location = new System.Drawing.Point(457, 156);
+            this.pictureBox1.Location = new System.Drawing.Point(499, 162);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(253, 178);
+            this.pictureBox1.Size = new System.Drawing.Size(276, 185);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -77,54 +75,29 @@
             this.Percentage.AutoSize = true;
             this.Percentage.Font = new System.Drawing.Font("Century Gothic", 14.14286F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Percentage.ForeColor = System.Drawing.Color.White;
-            this.Percentage.Location = new System.Drawing.Point(621, 411);
+            this.Percentage.Location = new System.Drawing.Point(677, 428);
             this.Percentage.Name = "Percentage";
             this.Percentage.Size = new System.Drawing.Size(46, 40);
             this.Percentage.TabIndex = 4;
             this.Percentage.Text = "%";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(886, 156);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(176, 40);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Anastasia";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 14.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(886, 240);
+            this.label3.Location = new System.Drawing.Point(967, 250);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(146, 40);
+            this.label3.Size = new System.Drawing.Size(0, 40);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Claudia";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 14.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(886, 319);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(128, 40);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Denisa";
             // 
             // Splash
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OrangeRed;
-            this.ClientSize = new System.Drawing.Size(1204, 554);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(1313, 577);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.Percentage);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
@@ -147,9 +120,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label Percentage;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
     }
 }
 
